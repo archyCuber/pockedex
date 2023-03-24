0a2349ab8e6969991244a9b53f79c323fef891fd
daniildenisov <denisov.v.daniil@gmail.com>
2023-03-24 08:56:22 +0300

    test commit

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,6 +1,6 @@
-import {Body, Controller, Post} from "@nestjs/common";
-import {CreateUserDto} from "./create-user.dto";
-import {UserService} from "./user.service";
+import { Body, Controller, Post } from "@nestjs/common";
+import { CreateUserDto } from "./create-user.dto";
+import { UserService } from "./user.service";
 
 @Controller('user')
 export class UserController{
@@ -9,6 +9,8 @@ export class UserController{
 
     @Post()
     async create(@Body() createUserDto: CreateUserDto) {
+        console.log('DD check');
+        
         await this.userService.create(createUserDto)
     }
 }
